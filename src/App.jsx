d015aca7139a8ba206b39f484c9f298f2596e5a4
Sebastian Valenzuela <svalenzuela.ortiz@gmail.com>
2023-09-05 18:01:02 -0300

    Listado css, table

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BaseColaboradores } from './assets/BaseColaboradores.js';
 import Listado from './components/Listado';
+import Formulario from './components/Formulario';
 
 
 function App() {
   
 
   return (
-    <>
-  <Listado colaboradores={BaseColaboradores}></Listado>
-    </>
+    <div className='container flex'>
+      {/* <div className='row'> */}
+        <div className='col-8'>
+          <Listado colaboradores={BaseColaboradores}></Listado>
+        </div>
+        <div className='col-4'>
+            <Formulario></Formulario>
+        </div>
+      {/* </div> */}
+      
+    </div>
   )
 }
 
--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,27 +1,36 @@
-import React from 'react'
+import Table from 'react-bootstrap/Table';
 
 const Listado = ({colaboradores}) => {
     console.log(colaboradores)
   return (
     <div>
-                <table>
+        <Table striped bordered hover size="lg">
+                <thead>
                     <th>id</th>
                     <th>Nombre</th>
-                    <tbody>        
+                    <th>Correo</th>
+                    <th>Edad</th>
+                    <th>Cargo</th>
+                    <th>Telefono</th>
+                </thead>
+                <tbody>        
         { 
         colaboradores.map( c =>  
             (
                 
-
                         <tr>
                             <td>{c.id}</td>
                             <td>{c.nombre}</td>
+                            <td>{c.correo}</td>
+                            <td>{c.edad}</td>
+                            <td>{c.cargo}</td>
+                            <td>{c.telefono}</td>
                         </tr>
             )
         ) 
         }
-            </tbody>
-        </table>
+                </tbody>
+        </Table>
     </div>
   )
 }
